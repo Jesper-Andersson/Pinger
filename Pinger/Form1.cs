05b4a19e357114696b9e3443d184bee8b38c747c
Jesper Andersson <ajesper00@gmail.com>
2017-11-19 01:57:53 +0100

    I dunno

--- Pinger/Form1.cs
+++ Pinger/Form1.cs
@@ -12,17 +12,17 @@ using System.Net.NetworkInformation;
 
 namespace Pinger
 {
-    public partial class Pinger : Form
-    {
-        public Pinger()
-        {
-            InitializeComponent();
-        }
+	public partial class Pinger : Form
+	{
+		public Pinger()
+		{
+			InitializeComponent();
+		}
 
 		//Declarations
 		public static bool Running = false;
-        public static bool pingable = false;
-        public static string ip;
+		public static bool pingable = false;
+		public static string ip;
 		static System.Windows.Forms.Timer myTimer = new System.Windows.Forms.Timer();
 		static int alarmCounter = 1;
 		static bool exitFlag = false;
@@ -31,7 +31,6 @@ namespace Pinger
 		{
 			myTimer.Stop();
 
-			// Displays a message box asking whether to continue running the timer.
 			if (Running == true)
 			{
 				// Restarts the timer and increments the counter.
@@ -47,65 +46,68 @@ namespace Pinger
 
 		//IPBox
 		private void textBox1_TextChanged(object sender, EventArgs e)
-        {
+		{
 
-        }
+		}
 
-        //PingDelayBox(sec)
-        private void AmountBox_TextChanged(object sender, EventArgs e)
-        {
-            int pingDelay = int.Parse(PingDelayBox.Text);
-        }
+		//PingDelayBox(sec)
+		private void AmountBox_TextChanged(object sender, EventArgs e)
+		{
+			int pingDelay = int.Parse(PingDelayBox.Text);
+		}
 
-        //StartButton
-        private void button1_Click(object sender, EventArgs e)
-        {
-            ip = IPBox.Text;
-            int PingDelay = int.Parse(PingDelayBox.Text);
+		//StartButton
+		private void button1_Click(object sender, EventArgs e)
+		{
+			ip = IPBox.Text;
+			int PingDelay = int.Parse(PingDelayBox.Text);
 
-            Running = !Running;
+			Running = !Running;
 			myTimer.Tick += new EventHandler(TimerEventProcessor);
 
 			myTimer.Interval = PingDelay * 1000;
 			myTimer.Start();
-			
-			while (Running == true)
-            {
-				if (exitFlag == false)
+
+			if (Running)
+			{
+				if (exitFlag == false && ip != null)
 				{
+					Ping isPing = new Ping();
 					liveStatus.Image = Properties.Resources.IMGstatusOn;
 					StartButton.Text = "Stop";
-					Ping isPing = new Ping();
-					if (ip != null)
+
+					try
 					{
-						try
-						{
-							//PingReply reply = isPing.Send(ip);
-							//pingable = reply.Status == IPStatus.Success;
-						}
-						catch (PingException)
-						{
-							// Discard PingExceptions and return false;
-						}
-						if (pingable == true)
-						{
-							respondingStatus.Image = Properties.Resources.IMGstatusOn;
-						}
-						else
-						{
-							respondingStatus.Image = Properties.Resources.IMGstatusOff;
-						}
+						PingReply reply = isPing.Send(ip);
+						pingable = reply.Status == IPStatus.Success;
 					}
+					catch (PingException)
+					{
+						// Discard PingExceptions and return false;
+					}
+					if (pingable == true)
+					{
+						respondingStatus.Image = Properties.Resources.IMGstatusOn;
+					}
+					else
+					{
+						respondingStatus.Image = Properties.Resources.IMGstatusOff;
+					}
+					System.Threading.Thread.Sleep(50);
 				}
-            }
+			}
+			else
+			{
 			respondingStatus.Image = Properties.Resources.IMGstatusNeutral;
 			liveStatus.Image = Properties.Resources.IMGstatusOff;
+			
 			StartButton.Text = "Start";
 			pingable = false;
-        }
-        private void Pinger_Load(object sender, EventArgs e)
-        {
+			}
+		}
+		private void Pinger_Load(object sender, EventArgs e)
+		{
 
-        }
-    }
+		}
+	}
 }
